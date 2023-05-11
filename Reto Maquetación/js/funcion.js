8c5d5409b109ba43e8a93db571ade8777c8858c1
CesarSustaita <c.sustaita@outlook.com>
2023-05-10 23:42:38 -0600

    Reto
    En este repo se encuentra un nuevo diseño en la maquetación

---
+++ Reto Maquetación/js/funcion.js
@@ -0,0 +1,67 @@
+
+
+const descripciones = [
+    {
+        name: "AIA Barlovento",
+        description: "Departamentos",
+        location: "San Luis Potosí",
+        totalArea: "116.72",
+        mainImage: "https://res.cloudinary.com/doktbbr78/image/upload/v1683051411/rbeehdj0yjy8j0bg9bbq.jpg",
+        terrace: "13.12",
+        rooms: "2",
+        available: "false"
+    },
+    {
+        name: "AIA León",
+        description: "Casas",
+        location: "Nuevo León",
+        totalArea: "132",
+        mainImage: "https://res.cloudinary.com/doktbbr78/image/upload/v1683051411/rbeehdj0yjy8j0bg9bbq.jpg",
+        terrace: "0",
+        rooms: "3",
+        available: "true"
+    },
+    {
+        name: "AIA Verde",
+        description: "Departamentos",
+        location: "San Luis Potosí",
+        totalArea: "107",
+        mainImage: "https://res.cloudinary.com/doktbbr78/image/upload/v1683051411/rbeehdj0yjy8j0bg9bbq.jpg",
+        terrace: "15.2",
+        rooms: "2",
+        available: "false"
+    },
+    {
+        name: "AIA Roja",
+        description: "Departamentos",
+        location: "Guanajuato",
+        totalArea: "168.1",
+        mainImage: "https://res.cloudinary.com/doktbbr78/image/upload/v1683051411/rbeehdj0yjy8j0bg9bbq.jpg",
+        terrace: "23.2",
+        rooms: "3",
+        available: "false"
+    },
+    {
+        name: "AIA Azul",
+        description: "Departamentos",
+        location: "an Luis Potosí",
+        totalArea: "132",
+        mainImage: "https://res.cloudinary.com/doktbbr78/image/upload/v1683051411/rbeehdj0yjy8j0bg9bbq.jpg",
+        terrace: "14.8",
+        rooms: "2",
+        available: "true"
+    },
+];
+
+
+const descripcionesController = document.getElementById("card");
+
+for (let i = 0; i < descripciones.length; i++) {
+    const propiedad = descripciones[i];
+/*
+    const cardDiv = document.createElement("id");
+    cardDiv.classList.add("img");
+
+*/
+
+}
